95f0531cf06f254c38e847203c60b37111127646
kajal-lufha <42331714+kajal-lufha@users.noreply.github.com>
2023-03-15 00:23:56 -0400

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_lb_backend_address_pool" "pool" {
   count               = 2
   name                = "pool-${count.index + 1}"
   loadbalancer_id     = azurerm_lb.lb.id
- # backend_ip_addresses = var.linux_vm_ids
+ 
 }
 
 resource "azurerm_lb_probe" "probe" {
